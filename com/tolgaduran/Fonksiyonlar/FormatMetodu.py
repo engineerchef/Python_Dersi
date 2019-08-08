705bad4eaa780321b783019c0116399d397089e0
Tolga DURAN <tolgaduran1980@gmail.com>
2019-08-08 16:07:02 +0300

    format() fonksiyonunun kullanımı

--- com/tolgaduran/Fonksiyonlar/FormatMetodu.py
+++ com/tolgaduran/Fonksiyonlar/FormatMetodu.py
@@ -9,3 +9,26 @@ yas = input("Yaşınız : ")
 
 kullanici = "{} {} {} yaşındadır.".format(ad, soyad, yas)
 print(kullanici)
+
+# Benzer şekilde aşağıdaki örnekte olduğu gibi verilen bir hark dizisindeki her harfi ayrı ayrı yazdırmak için
+# for döngüsünü kullanarak formatlama işlemini fonksiyon kullanarak yapalım.
+harfler = "abcçdefgğhıijklmnoöpqrsştuüvwxyz"
+print("\nHarflerin format() fonskiyonu kullanılarak sırayla yazılışı\n")
+
+
+def harfFonksiyonu():
+    for i in harfler:
+        print("Harf : {}".format(i))
+
+
+harfFonksiyonu()
+
+
+# Ya da Listeler kullanılarak da dizi içerisindekileri şu şekilde yazdırabiliriz
+def listeFonksiyonu(*args):
+    for i in args:
+        print("İçerik : {}".format(i))
+
+
+print("\nListe içerik\n")
+listeFonksiyonu("Tolga", "DURAN", 38)
