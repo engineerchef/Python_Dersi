e19ef6250a9f72eb22ce85b8143aff03c699e257
Tolga DURAN <tolgaduran1980@gmail.com>
2019-08-08 15:15:58 +0300

    **kwargs ve *args değişken parametresi kullanımı Detaylı Bilgi İçin : https://www.fast.ai/2019/08/06/delegation/

--- com/tolgaduran/Fonksiyonlar/CokluDegiskenArgumanFonksiyonu.py
+++ com/tolgaduran/Fonksiyonlar/CokluDegiskenArgumanFonksiyonu.py
@@ -1,6 +1,7 @@
 # Çoklu değişken tanımlayacağımız zamanlarda
 # *args parametresi ile fonksiyon oluşturmamız fonksiyonun tekrarlabilirliği açısından oldukça önemlidir
 # Bu tür bir fonksiyon tanımlamak istediğimizde içerik olarak her tür değişken tipini kullanabiliriz.
+# Detaylı Bilgi İçin : https://www.fast.ai/2019/08/06/delegation/
 
 def coklu(*args):
     for i in args:
--- com/tolgaduran/Fonksiyonlar/CokluDictArgumanFonksiyonu.py
+++ com/tolgaduran/Fonksiyonlar/CokluDictArgumanFonksiyonu.py
@@ -1,7 +1,8 @@
 # Çoklu dictionary kullanmak istediğimiz zamanlarda
 # **kwargs parametresi ile istediğimiz kadar değişkeni kullanacağımız fonksiyon için tanımlayabiliriz
 # Ancak tanımlama yapılırken <K,V> yani anahtar-değişken şeklinde yamamız gerekir.
-# Bu durumda fonksiyonumuz dict type dizi olacaktır
+# Bu durumda fonksiyonumuz dict type dizi olacaktır.
+# Detaylı Bilgi İçin : https://www.fast.ai/2019/08/06/delegation/
 
 def coklu(**kwargs):
     for key, value in kwargs.items():
