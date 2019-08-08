e3562b67869e73ea4277d13871f95924112486aa
Tolga DURAN <tolgaduran1980@gmail.com>
2019-08-08 16:24:50 +0300

    format() fonksiyonunun kullanımı

--- com/tolgaduran/Fonksiyonlar/FormatMetodu.py
+++ com/tolgaduran/Fonksiyonlar/FormatMetodu.py
@@ -1,6 +1,6 @@
-ad = input("Adınız : ")
-soyad = input("Soyadınız : ")
-yas = input("Yaşınız : ")
+ad = str(input("Adınız : "))
+soyad = str(input("Soyadınız : "))
+yas = int(input("Yaşınız : "))
 
 # Yukarıda kullanıcıdan girmesini istediğimiz ad, soyad ve yaş bilgilerine bağlı olarak normalde ekran çıktımızı
 # print(ad + " " + soyad + " " + " " + yas + " yaşındadır.") şeklinde ifade ediyorduk.
@@ -8,6 +8,33 @@ yas = input("Yaşınız : ")
 # tanımlayabileceğimiz gibi aşağıdaki şekilde de bir değişkene tanımlayarak da kullanabiliriz.
 
 kullanici = "{} {} {} yaşındadır.".format(ad, soyad, yas)
+print(kullanici)
+
+# İçerik belirleyerek de kullanıcı bilgilerini yaxdırabiliriz
+kullanici = "{name} {surname} {age} yaşındadır.".format(name=ad, surname=soyad, age=yas)
+print(kullanici)
+
+# Veya içerik sırasını değiştirebiliriz
+kullanici = "{1} {0} {2} yaşındadır.".format(ad, soyad, yas)
+print(kullanici)
+
+# Sağa yaslama işlemi
+kullanici = "{:>15} {} {} yaşındadır.".format(ad, soyad, yas)
+print(kullanici)
+
+# Sola yaslama işlemi
+kullanici = "{:<15} {} {} yaşındadır.".format(ad, soyad, yas)
+print(kullanici)
+
+# Ortalama işlemi
+kullanici = "{:^15} {} {} yaşındadır.".format(ad, soyad, yas)
+print(kullanici)
+
+# Belirtilen yerlere belirtilen ifadelere uygun olacak şekilde string ve sayısal ifade gelmesi için
+# :s => String
+# :d => Sayısal
+# belirteçleri kullanabiliriz.
+kullanici = "{:s} {:s} {:d} yaşındadır.".format(ad, soyad, yas)
 print(kullanici)
 
 # Benzer şekilde aşağıdaki örnekte olduğu gibi verilen bir hark dizisindeki her harfi ayrı ayrı yazdırmak için
